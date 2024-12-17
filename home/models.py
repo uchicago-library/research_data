ff04a5e38dcf901a0f88bec4eea7dbfeb7523273
Vitor Gonçalves <vitorg@uchicago.edu>
2024-12-16 20:43:23 -0600

    add a conditional background overlay

--- home/models.py
+++ home/models.py
@@ -7,15 +7,19 @@ from wagtail.fields import RichTextField, StreamField
 class HomePage(AbstractBasePage):
     subpage_types = ['base.StandardPage', 'news.NewsIndexPage']
 
-    cta_heading = models.CharField(max_length=100, blank=True)
-    cta_text = RichTextField(blank=True, features=['bold'])
-    cta_button_text = models.CharField(max_length=100, blank=True)
+    cta_heading = models.CharField(
+        verbose_name="Heading",
+        max_length=100, blank=True)
+    cta_text = RichTextField(verbose_name="Text",blank=True, features=['bold'])
+    cta_button_text = models.CharField(verbose_name="Button Text",max_length=100, blank=True)
     cta_button_link = models.URLField(
+        verbose_name="Button external link override",
         blank=True,
         help_text="Overrides the page link.",
     )
     cta_button_page = models.ForeignKey(
         'wagtailcore.Page',
+        verbose_name="Button internal page link",
         null=True,
         blank=True,
         related_name='+',
@@ -24,11 +28,17 @@ class HomePage(AbstractBasePage):
     )
     cta_background_image = models.ForeignKey(
         "wagtailimages.Image",
+        verbose_name="CTA section background image.",
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
         related_name="+",
         help_text="Image to display as a background for the Call to Action. Mind the contrast, and for different screen sizes.",
+    )
+    cta_darken_image = models.BooleanField(
+        default=False,
+        verbose_name="Darken background image?",
+        help_text="Check this box to add an overlay to the background image to darken it by 20%."
     )
     sections = StreamField(
         [
@@ -48,8 +58,9 @@ class HomePage(AbstractBasePage):
                     FieldPanel('cta_button_link'),
                     FieldPanel('cta_button_page'),
                     FieldPanel('cta_background_image'),
+                    FieldPanel('cta_darken_image'),
                 ],
-                heading='Call to Action Panel',
+                heading='Call to Action Section (CTA)',
             ),
         ]
         + AbstractBasePage.content_panels[1:]
