d5f8b8c7fdeca2ffed55a5351ffb67cab2e12819
Vitor Gonçalves <vitorg@uchicago.edu>
2024-12-20 13:50:16 -0600

    fancy linked image

--- base/models.py
+++ base/models.py
@@ -1,6 +1,8 @@
 # from django.db import models
 from django.db import models
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel, PageChooserPanel
+from wagtail.documents.blocks import DocumentChooserBlock
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.blocks import (
     CharBlock,
     IntegerBlock,
@@ -8,6 +10,7 @@ from wagtail.blocks import (
     RichTextBlock,
     StreamBlock,
     StructBlock,
+    URLBlock,
 )
 from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
 from wagtail.fields import StreamField
@@ -53,6 +56,37 @@ class LinkFields(models.Model):
     class Meta:
         abstract = True
 
+class ImageLink(StructBlock):
+    """
+    Normal image for web exhibits.
+    """
+
+    link_image = ImageChooserBlock(required=False)
+    alt_text = CharBlock(
+        required=False,
+        help_text='Required if no link text supplied for ADA compliance',
+    )
+    link_text = CharBlock(
+        required=False,
+        help_text='Text to display below the image',
+    )
+    link_page = PageChooserBlock(required=False)
+    link_document = DocumentChooserBlock(required=False)
+    link_external = URLBlock(required=False)
+
+    # Define the order of fields in the admin interface
+    content_panels = [
+        FieldPanel('link_image'),
+        FieldPanel('alt_text'),
+        FieldPanel('link_text'),
+        FieldPanel('link_page'),
+        FieldPanel('link_document'),
+        FieldPanel('link_external'),
+    ]
+
+    class Meta:
+        icon = 'image'
+        template = 'base/blocks/image_link.html'
 
 class Logo(LinkFields):
     """
@@ -108,6 +142,12 @@ class DefaultBodyFields(StreamBlock):
     h5 = CharBlock(
         icon='title',
         template='base/blocks/h5.html',
+    )
+    
+    image_link = ImageLink(
+        label="Linked Image",
+        help_text='A fancy link made out of a thumbnail and simple text. It will link to either the page, the document, or to the external, depending on the first to be non-empty.',
+        group="Links",
     )
 
     class Meta:
